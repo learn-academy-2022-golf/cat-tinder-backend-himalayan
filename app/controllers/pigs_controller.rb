e12ccc8bda3232c3e5a788cad0b7b2651469cb36
Charlie19255 <charlie19255@gmail.com>
2022-12-15 14:03:38 -0800

    Goals complete

--- app/controllers/pigs_controller.rb
+++ app/controllers/pigs_controller.rb
@@ -1,2 +1,34 @@
 class PigsController < ApplicationController
+    def index
+        pigs = Pig.all
+        render json: pigs
+    end
+  
+    def create
+        pig = Pig.create(pig_params)
+        render json: pig
+    end
+  
+    def update
+        pig = Pig.find(params[:id])
+        pig.update(pig_params)
+        if pig.valid?
+            render json: pig
+        else
+            render json: pig.errors, status: :unprocessable_entity
+        end
+    end
+  
+    def destroy
+        pig = Pig.find(params[:id])
+        if pig.destroy
+                render json: pig
+        else
+                render json: pig.errors, status: :unprocessable_entity
+        end
+    end
+    private
+    def pig_params
+      params.require(:pig).permit(:name, :breed, :habits, :image)
+    end
 end
--- spec/requests/pigs_spec.rb
+++ spec/requests/pigs_spec.rb
@@ -2,6 +2,97 @@ require 'rails_helper'
 
 RSpec.describe "Pigs", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of guinea pigs" do
+      Pig.create(
+        name: 'Mike the Pig',
+        breed: 'Teddy Guinea Pig',
+        habits: 'Helping their human with their experiments',
+        image: 'https://www.istockphoto.com/es/foto/preparado-cuy-guinea-pig-gm497034064-78906333'
+      )
+
+      # Make a request
+      get '/pigs'
+
+      pig = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(pig.length).to eq 1
+    end
+  end
+  describe "POST /create" do
+    it "creates a pig" do
+      # The params we are going to send with the request
+      pig_params = {
+        pig: {
+          name: 'Mike the Pig',
+          breed: 'Teddy Guinea Pig',
+          habits: 'Helping their human with their experiments',
+          image: 'https://www.istockphoto.com/es/foto/preparado-cuy-guinea-pig-gm497034064-78906333'
+        }
+      }
+  
+      # Send the request to the server
+      post '/pigs', params: pig_params
+  
+      # Assure that we get a success back
+      expect(response).to have_http_status(200)
+  
+      # Look up the cat we expect to be created in the db
+      pig = Pig.last
+  
+      # Assure that the created cat has the correct attributes
+      expect(pig.name).to eq 'Mike the Pig'
+    end
+  end
+  describe "PATCH /update" do
+    it "adds changes to a pig" do
+
+      Pig.create(
+        name: 'Mike the Pig',
+        breed: 'Teddy Guinea Pig',
+        habits: 'Helping their human with their experiments',
+        image: 'https://www.istockphoto.com/es/foto/preparado-cuy-guinea-pig-gm497034064-78906333'
+      )
+      pig_params = {
+        pig: {
+          name: 'Mike the One and Only Pig',
+          breed: 'Teddy Guinea Pig',
+          habits: 'Helping their human with their experiments',
+          image: 'https://www.istockphoto.com/es/foto/preparado-cuy-guinea-pig-gm497034064-78906333'
+        }
+      }
+  
+      # Send the request to the server
+      pig = Pig.last
+      patch "/pigs/#{pig.id}", params: pig_params
+  
+      # Assure that we get a success back
+      expect(response).to have_http_status(200)
+  
+      # Look up the cat we expect to be created in the db
+      pig = Pig.last
+  
+      # Assure that the created cat has the correct attributes
+      expect(pig.name).to eq 'Mike the One and Only Pig'
+    end
+  end
+  describe "DELETE /destroy" do
+    it "destroys a requested pig" do
+    #  pig = Pig.new(having_attributes)
+    #   pig.save
+      
+     
+    #   expect do delete pig_url(pig) 
+    #   end.to change(Pig.all, :count).by(-1)
+    pig = Pig.create(
+      name: 'Mike the Pig',
+      breed: 'Teddy Guinea Pig',
+      habits: 'Helping their human with their experiments',
+      image: 'https://www.istockphoto.com/es/foto/preparado-cuy-guinea-pig-gm497034064-78906333'
+    )
+    pig = Pig.last
+    delete "/pigs/#{pig.id}"
+      expect(response).to have_http_status(200)
+    expect(Pig.all).to eq []
+    end
   end
 end
